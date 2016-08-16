bbbf1e56a7fc3f3fd3de1a7a43b4285c90cb8ac6
Willem van Zantvoort <criccle@gmail.com>
2016-08-16 14:48:10 +0200

    Initial upload
    Removed unnecessary functions

---
+++ src/webRTC/widget/webRTC.js
@@ -0,0 +1,103 @@
+/*global logger*/
+/*
+    webRTC
+    ========================
+
+    @file      : webRTC.js
+    @version   : 1.0.0
+    @author    : Willem van Zantvoort
+    @date      : 2016-08-16
+    @copyright : <Your Company> 2016
+    @license   : Apache 2
+
+    Documentation
+    ========================
+    Describe your widget here.
+*/
+
+// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "dijit/_TemplatedMixin",
+
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/dom-prop",
+    "dojo/dom-geometry",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/html",
+    "dojo/_base/event",
+
+    "webRTC/lib/jquery-1.11.2",
+    "dojo/text!webRTC/widget/template/webRTC.html"
+], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent, _jQuery, widgetTemplate) {
+    "use strict";
+
+    var $ = _jQuery.noConflict(true);
+
+    // Declare widget's prototype.
+    return declare("webRTC.widget.webRTC", [ _WidgetBase, _TemplatedMixin ], {
+        // _TemplatedMixin will create our dom node using this HTML template.
+        templateString: widgetTemplate,
+
+        // DOM elements
+
+        // Parameters configured in the Modeler.
+
+        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+        _contextObj: null,
+        _alertDiv: null,       
+
+        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+        constructor: function () {
+            logger.debug(this.id + ".constructor");
+        },
+
+        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+        postCreate: function () {
+            logger.debug(this.id + ".postCreate");
+
+            this._setupEvents();
+        },
+
+        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+        update: function (obj, callback) {
+            logger.debug(this.id + ".update");
+
+            this._contextObj = obj;
+            this._updateRendering(callback); // We're passing the callback to updateRendering to be called after DOM-manipulation
+        },
+
+        // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
+        resize: function (box) {
+          logger.debug(this.id + ".resize");
+        },
+
+        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+        uninitialize: function () {
+          logger.debug(this.id + ".uninitialize");
+            // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
+        },
+
+        // Attach events to HTML dom elements
+        _setupEvents: function () {
+            logger.debug(this.id + "._setupEvents");
+        },
+
+        // Rerender the interface.
+        _updateRendering: function (callback) {
+            logger.debug(this.id + "._updateRendering");
+
+            // The callback, coming from update, needs to be executed, to let the page know it finished rendering
+            mendix.lang.nullExec(callback);
+        },
+    });
+});
+
+require(["webRTC/widget/webRTC"]);
